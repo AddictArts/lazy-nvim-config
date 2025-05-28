ffcfb2c21359999f642a782b4ef961b5dc891ba2
John Pywtorak <john.pywtorak@sri.com>
2025-05-27 23:09:58 -0700

    Added defualt mason ensure install

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "jdtls",
+      "lemminx",
+      "stylua",
+      "shellcheck",
+      "shfmt",
+    },
+  },
+}
